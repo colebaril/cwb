04dbf6ac806d069108cbc6dd30894abb07704994
Cole Baril <cole@colebaril.ca>
2026-01-30 20:08:33 -0600

    rev_caption

--- R/add_caption_cwb.R
+++ R/add_caption_cwb.R
@@ -1,49 +1,54 @@
-#' Generate social caption for ggplot or gt table
+#' Adds a custom caption to plots
 #'
-#' Returns a caption string with optional data source and social icons,
-#' suitable for passing directly to labs(caption = …) in ggplot.
+#' Add a social media caption to a ggplot or gt table.
 #'
-#' @param include_data_source Logical; whether to include data source
-#' @param data_source Character; the source of the data
-#' @param github_username Character; GitHub username (default: "colebaril")
-#' @param bluesky_username Character; Bluesky username (default: "@colebaril.ca")
-#' @return Character string containing HTML caption
+#' @param type Either "plot" or "table"
+#' @param github_username Your GitHub username (default: "colebaril")
+#' @param include_data_source Boolean; whether to include data source
+#' @param data_source The source of the data
+#' @param bluesky_username Your Bluesky username 
+#' @return A ggplot or gt object with a caption applied
 #' @export
-social_caption <- function(include_data_source = FALSE,
-                           data_source = NULL,
-                           github_username = "colebaril",
-                           bluesky_username = "@colebaril.ca") {
+#' @import ggplot2
+#' @import gt
+#' @import glue
+#' @import sysfonts
+#' @import showtext
+#' @import ggtext
+add_caption_cwb <- function(type = c("plot", "table"), 
+                     include_data_source = FALSE,
+                     data_source = NULL,
+                     github_username = "colebaril",
+                     bluesky_username = "@colebaril.ca") {
+  type <- match.arg(type)
   
-  # Load fonts for icons
-  sysfonts::font_add(
-    family = "Font Awesome 7 Brands", 
-    regular = "D:/Projects/fa/fontawesome-free-7.0.1-desktop/otfs/Font Awesome 7 Brands-Regular-400.otf"
-  )
+
+  sysfonts::font_add( family = "Font Awesome 7 Brands", 
+                      regular = "D:/Projects/fa/fontawesome-free-7.0.1-desktop/otfs/Font Awesome 7 Brands-Regular-400.otf") 
   showtext::showtext_auto()
   
-  # Font Awesome icons
   github_icon <- "\uf09b"  
   bluesky_icon <- "\ue671"
-  
-  # Base caption
-  caption <- glue::glue(
-    "<span style='font-weight:bold; color:#4d4d4d;'>Graphic:</span>
-     <span style='font-family:\"Font Awesome 7 Brands\"; color:#4d4d4d;'>{github_icon}</span>
-     <span style='color:#4d4d4d;'>{github_username}</span>
-     <span style='margin-left:15px;'></span>
-     <span style='font-family:\"Font Awesome 7 Brands\"; color:#4d4d4d;'>{bluesky_icon}</span>
-     <span style='color:#4d4d4d;'>{bluesky_username}</span>"
+  social_caption <- glue::glue(
+    "<span style='font-weight:bold; color:#4d4d4d; '>Graphic:</span>
+  <span style='font-family:\"Font Awesome 7 Brands\"; color:#4d4d4d;'>{github_icon}</span>
+   <span style='color:#4d4d4d;'>{github_username}</span>
+   <span style='margin-left:15px;'></span>
+   <span style='font-family:\"Font Awesome 7 Brands\"; color:#4d4d4d;'>{bluesky_icon}</span>
+   <span style='color:#4d4d4d;'>{bluesky_username}</span>"
   )
   
-  # Add Data source if requested
   if (include_data_source && !is.null(data_source)) {
-    caption <- glue::glue("{caption}<br>
-      <span style='font-weight:bold; color:#4d4d4d;'>Data:</span>
-      <span style='color:#4d4d4d;'>{data_source}</span>")
+    social_caption <- glue::glue("{social_caption}  
+    <span style='font-weight:bold; color:#4d4d4d;'>Data:</span> <span style='color:#4d4d4d;'>{data_source}</span>")
   }
   
-  # Remove any accidental literal newlines
-  caption <- stringr::str_replace_all(caption, "\n", "")
-  
-  return(caption)
+  if (type == "plot") {
+    return(list(
+      labs(caption = social_caption),
+      theme(plot.caption = ggtext::element_textbox_simple(size = 12, face = "bold"))
+    ))
+  } else if (type == "table") {
+    return(function(x) gt::tab_source_note(x, gt::md(social_caption)))
+  }
 }
